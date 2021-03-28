2a38be775c1e840290e1b47ae0440704a433d117
Hani <hani.sabeen03222@gmail.com>
2021-03-28 11:31:35 -0800

    Calculator

---
+++ app.js
@@ -0,0 +1,14 @@
+function getNumber(num) {
+      var result = document.getElementById("result");
+      result.value += num}
+
+
+      function clearAll() {
+          var result = document.getElementById("result");
+          result.value=""
+        }
+
+          function getResult(){
+               var result = document.getElementById("result");
+               result.value = eval (result.value)
+            }
